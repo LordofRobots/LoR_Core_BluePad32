89e5b3d54cd35c3b434d7f37d42638564600bf8b
Lord of Robots <131721361+LordofRobots@users.noreply.github.com>
2024-04-29 17:10:28 -0400

    Initial upload
    first upload

---
+++ LoR_Core_BluePad32/LoR_Core_BluePad32.ino
@@ -0,0 +1,364 @@
+#include <Bluepad32.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+
+#define CHANNEL 0
+
+// IO Interface Definitions
+#define LED_DataPin 12
+#define LED_COUNT 36
+#define SwitchPin 34
+#define MotorEnablePin 13
+#define channel1Pin 16
+#define channel2Pin 17
+#define channel3Pin 21
+#define channel4Pin 22
+
+// Motor Pin Definitions
+#define motorPin_M1_A 26
+#define motorPin_M1_B 18
+#define motorPin_M2_A 14
+#define motorPin_M2_B 5
+#define motorPin_M3_A 15
+#define motorPin_M3_B 33
+#define motorPin_M4_A 23
+#define motorPin_M4_B 19
+#define motorPin_M5_A 25
+#define motorPin_M5_B 27
+#define motorPin_M6_A 4
+#define motorPin_M6_B 32
+#define MotorEnablePin 13
+
+const int motorPins_A[] = { motorPin_M1_A, motorPin_M2_A, motorPin_M3_A, motorPin_M4_A, motorPin_M5_A, motorPin_M6_A };
+const int motorPins_B[] = { motorPin_M1_B, motorPin_M2_B, motorPin_M3_B, motorPin_M4_B, motorPin_M5_B, motorPin_M6_B };
+
+// Motor PWM Configuration Definitions
+const int Motor_M1_A = 0;
+const int Motor_M1_B = 1;
+const int Motor_M2_A = 2;
+const int Motor_M2_B = 3;
+const int Motor_M3_A = 4;
+const int Motor_M3_B = 5;
+const int Motor_M4_A = 6;
+const int Motor_M4_B = 7;
+const int Motor_M5_A = 8;
+const int Motor_M5_B = 9;
+const int Motor_M6_A = 10;
+const int Motor_M6_B = 11;
+const int MOTOR_PWM_Channel_A[] = { Motor_M1_A, Motor_M2_A, Motor_M3_A, Motor_M4_A, Motor_M5_A, Motor_M6_A };
+const int MOTOR_PWM_Channel_B[] = { Motor_M1_B, Motor_M2_B, Motor_M3_B, Motor_M4_B, Motor_M5_B, Motor_M6_B };
+const int PWM_FREQUENCY = 20000;
+const int PWM_RESOLUTION = 8;
+
+// Process joystick input and calculate motor speeds
+const int DEAD_BAND = 20;
+const float TURN_RATE = 1.5;
+int Motor_FrontLeft_SetValue, Motor_FrontRight_SetValue, Motor_BackLeft_SetValue, Motor_BackRight_SetValue = 0;
+void Motion_Control(int LY_Axis, int LX_Axis, int RX_Axis) {
+  int FrontLeft_TargetValue, FrontRight_TargetValue, BackLeft_TargetValue, BackRight_TargetValue = 0;
+  int ForwardBackward_Axis = LY_Axis;
+  int TurnLeftRight_Axis = -RX_Axis;
+
+  //Set deadband
+  if (abs(ForwardBackward_Axis) < DEAD_BAND) ForwardBackward_Axis = 0;
+  if (abs(TurnLeftRight_Axis) < DEAD_BAND) TurnLeftRight_Axis = 0;
+
+  //Calculate strafe values
+  FrontLeft_TargetValue = -ForwardBackward_Axis;
+  BackLeft_TargetValue = -ForwardBackward_Axis;
+  FrontRight_TargetValue = ForwardBackward_Axis;
+  BackRight_TargetValue = ForwardBackward_Axis;
+
+  //calculate rotation values
+  if (abs(TurnLeftRight_Axis) > DEAD_BAND) {
+    FrontLeft_TargetValue += (TURN_RATE * TurnLeftRight_Axis);
+    BackLeft_TargetValue += (TURN_RATE * TurnLeftRight_Axis);
+    FrontRight_TargetValue += (TURN_RATE * TurnLeftRight_Axis);
+    BackRight_TargetValue += (TURN_RATE * TurnLeftRight_Axis);
+  }
+
+  //constrain to joystick range
+  FrontLeft_TargetValue = constrain(FrontLeft_TargetValue, -127, 127);
+  BackLeft_TargetValue = constrain(BackLeft_TargetValue, -127, 127);
+  FrontRight_TargetValue = constrain(FrontRight_TargetValue, -127, 127);
+  BackRight_TargetValue = constrain(BackRight_TargetValue, -127, 127);
+
+  //set motor speed through slew rate function
+  Motor_FrontLeft_SetValue = SlewRateFunction(FrontLeft_TargetValue, Motor_FrontLeft_SetValue);
+  Motor_FrontRight_SetValue = SlewRateFunction(FrontRight_TargetValue, Motor_FrontRight_SetValue);
+  Motor_BackLeft_SetValue = SlewRateFunction(BackLeft_TargetValue, Motor_BackLeft_SetValue);
+  Motor_BackRight_SetValue = SlewRateFunction(BackRight_TargetValue, Motor_BackRight_SetValue);
+}
+
+// Function to handle slew rate for motor speed ramping
+// Slew rate for ramping motor speed
+const int SLEW_RATE_MS = 1;
+int SlewRateFunction(int Input_Target, int Input_Current) {
+  int speedDiff = Input_Target - Input_Current;
+  if (speedDiff > 0) Input_Current += min(speedDiff, SLEW_RATE_MS);
+  else if (speedDiff < 0) Input_Current -= min(-speedDiff, SLEW_RATE_MS);
+  Input_Current = constrain(Input_Current, -127, 127);
+  return Input_Target;  //// BYPASSED
+}
+
+// Function to control motor output based on input values
+// Motor speed limits and starting speed
+const int MAX_SPEED = 255;
+const int MIN_SPEED = -255;
+const int MIN_STARTING_SPEED = 140;
+const int STOP = 0;
+bool INVERT = false;
+void Set_Motor_Output(int Output, int Motor_ChA, int Motor_ChB) {
+  if (INVERT) Output = -Output;
+  Output = constrain(Output, -127, 127);
+  int Mapped_Value = map(abs(Output), 0, 127, MIN_STARTING_SPEED, MAX_SPEED);
+  int A, B = 0;
+  if (Output < -DEAD_BAND) {  // Rotate Clockwise
+    A = 0;
+    B = Mapped_Value;
+  } else if (Output > DEAD_BAND) {  // Rotate Counter-Clockwise
+    A = Mapped_Value;
+    B = 0;
+  } else {  // Rotation Stop
+    A = STOP;
+    B = STOP;
+  }
+  ledcWrite(Motor_ChA, A);  //send to motor control pins
+  ledcWrite(Motor_ChB, B);
+}
+
+// configure motor output
+void Motor_Control() {
+  Set_Motor_Output(Motor_FrontLeft_SetValue, Motor_M1_A, Motor_M1_B);
+  Set_Motor_Output(Motor_BackLeft_SetValue, Motor_M2_A, Motor_M2_B);
+  Set_Motor_Output(Motor_FrontRight_SetValue, Motor_M5_A, Motor_M5_B);
+  Set_Motor_Output(Motor_BackRight_SetValue, Motor_M6_A, Motor_M6_B);
+}
+
+// stop motors from spinning
+void Motor_STOP() {
+  Set_Motor_Output(STOP, Motor_M1_A, Motor_M1_B);
+  Set_Motor_Output(STOP, Motor_M2_A, Motor_M2_B);
+  Set_Motor_Output(STOP, Motor_M5_A, Motor_M5_B);
+  Set_Motor_Output(STOP, Motor_M6_A, Motor_M6_B);
+}
+
+// Tones created in the motors. Cycle through each motor.
+void Start_Tone() {
+  for (int i = 0; i < 6; i++) {
+    long ToneTime = millis() + 200;
+    bool state = 0;
+    while (millis() < ToneTime) {
+      digitalWrite(motorPins_A[i], state);
+      digitalWrite(motorPins_B[i], !state);
+      state = !state;
+      long WaitTime = micros() + (100 * (i + 1));
+      while (micros() < WaitTime) {}
+    }
+    digitalWrite(motorPins_A[i], 0);
+    digitalWrite(motorPins_B[i], 0);
+    delay(50);
+  }
+}
+
+// Set a specific color for the entire NeoPixel strip
+// NeoPixel Configurations
+Adafruit_NeoPixel strip(LED_COUNT, LED_DataPin, NEO_GRB + NEO_KHZ800);
+const uint32_t RED = strip.Color(255, 0, 0, 0);
+const uint32_t GREEN = strip.Color(0, 255, 0, 0);
+const uint32_t BLUE = strip.Color(0, 0, 255, 0);
+const uint32_t WHITE = strip.Color(0, 0, 0, 255);
+const uint32_t PURPLE = strip.Color(255, 0, 255, 0);
+const uint32_t CYAN = strip.Color(0, 255, 255, 0);
+const uint32_t YELLOW = strip.Color(255, 255, 0, 0);
+void NeoPixel_SetColour(uint32_t color) {
+  for (int i = 0; i < strip.numPixels(); i++) {  // For each pixel in strip...
+    strip.setPixelColor(i, color);               //  Set pixel's color (in RAM)
+    strip.show();                                // Update strip with new contents
+  }
+}
+
+ControllerPtr myController = nullptr;  // Define a single controller pointer
+
+void onConnectedController(ControllerPtr ctl) {
+  if (myController == nullptr) {
+    Serial.println("Controller connected");
+    myController = ctl;  // Assign the connected controller to the single pointer
+  } else {
+    Serial.println("Another controller tried to connect but is rejected");
+    ctl->disconnect();  // Reject the connection if another controller tries to connect
+  }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  if (myController == ctl) {
+    Serial.println("Controller disconnected");
+    myController = nullptr;  // Reset the controller pointer when disconnected
+  }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+  Serial.printf(
+    "idx=%d, dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, %4d, brake: %4d, throttle: %4d, "
+    "misc: 0x%02x, gyro x:%6d y:%6d z:%6d, accel x:%6d y:%6d z:%6d\n",
+    ctl->index(),        // Controller Index
+    ctl->dpad(),         // D-pad
+    ctl->buttons(),      // bitmask of pressed buttons
+    ctl->axisX(),        // (-511 - 512) left X Axis
+    ctl->axisY(),        // (-511 - 512) left Y axis
+    ctl->axisRX(),       // (-511 - 512) right X axis
+    ctl->axisRY(),       // (-511 - 512) right Y axis
+    ctl->brake(),        // (0 - 1023): brake button
+    ctl->throttle(),     // (0 - 1023): throttle (AKA gas) button
+    ctl->miscButtons(),  // bitmask of pressed "misc" buttons
+    ctl->gyroX(),        // Gyro X
+    ctl->gyroY(),        // Gyro Y
+    ctl->gyroZ(),        // Gyro Z
+    ctl->accelX(),       // Accelerometer X
+    ctl->accelY(),       // Accelerometer Y
+    ctl->accelZ()        // Accelerometer Z
+  );
+}
+
+void processGamepad(ControllerPtr ctl) {
+  // There are different ways to query whether a button is pressed.
+  // By query each button individually:
+  //  a(), b(), x(), y(), l1(), etc...
+  if (ctl->a()) {
+    static int colorIdx = 0;
+    // Some gamepads like DS4 and DualSense support changing the color LED.
+    // It is possible to change it by calling:
+    switch (colorIdx % 3) {
+      case 0:
+        // Red
+        ctl->setColorLED(255, 0, 0);
+        break;
+      case 1:
+        // Green
+        ctl->setColorLED(0, 255, 0);
+        break;
+      case 2:
+        // Blue
+        ctl->setColorLED(0, 0, 255);
+        break;
+    }
+    colorIdx++;
+  }
+
+  if (ctl->b()) {
+    // Turn on the 4 LED. Each bit represents one LED.
+    static int led = 0;
+    led++;
+    // Some gamepads like the DS3, DualSense, Nintendo Wii, Nintendo Switch
+    // support changing the "Player LEDs": those 4 LEDs that usually indicate
+    // the "gamepad seat".
+    // It is possible to change them by calling:
+    ctl->setPlayerLEDs(led & 0x0f);
+  }
+
+  if (ctl->x()) {
+    // Duration: 255 is ~2 seconds
+    // force: intensity
+    // Some gamepads like DS3, DS4, DualSense, Switch, Xbox One S support
+    // rumble.
+    // It is possible to set it by calling:
+    ctl->playDualRumble(0x00 /* delay */, 0xc0 /* durationMs */, 0xc0 /* weakForce */, 0xc0 /* strongForce */);
+  }
+
+  // Another way to query controller data is by getting the buttons() function.
+  // See how the different "dump*" functions dump the Controller info.
+  dumpGamepad(ctl);
+}
+
+void processControllers() {
+  if (myController && myController->isConnected()) {
+    if (myController->isGamepad()) {
+      processGamepad(myController);
+    } else {
+      Serial.println("Unsupported controller");
+    }
+  }
+}
+
+// Set up pins, LED PWM functionalities and begin PS4 controller, Serial and Serial2 communication
+void setup() {
+  Serial.begin(115200);
+
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+  // Setup the Bluepad32 callbacks
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+
+  // "forgetBluetoothKeys()" should be called when the user performs
+  // a "device factory reset", or similar.
+  // Calling "forgetBluetoothKeys" in setup() just as an example.
+  // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+  // But it might also fix some connection / re-connection issues.
+  // BP32.forgetBluetoothKeys();
+
+  // Enables mouse / touchpad support for gamepads that support them.
+  // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+  // - First one: the gamepad
+  // - Second one, which is a "virtual device", is a mouse.
+  // By default, it is disabled.
+  BP32.enableVirtualDevice(false);
+
+  // Set up the pins
+  pinMode(LED_DataPin, OUTPUT);
+  pinMode(SwitchPin, INPUT_PULLUP);
+  pinMode(MotorEnablePin, OUTPUT);
+
+  //attachInterrupt(SwitchPin, handleInterrupt, CHANGE);
+
+  for (int i = 0; i < 6; i++) {
+    pinMode(motorPins_A[i], OUTPUT);
+    pinMode(motorPins_B[i], OUTPUT);
+    digitalWrite(motorPins_A[i], 0);
+    digitalWrite(motorPins_B[i], 0);
+  }
+
+  // output preset bias
+  digitalWrite(LED_DataPin, 0);
+  digitalWrite(MotorEnablePin, 1);
+
+  // Neopixels Configuration
+  strip.begin();            // INITIALIZE NeoPixel strip object
+  strip.show();             // Turn OFF all pixels ASAP
+  strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  // Motor test tones
+  NeoPixel_SetColour(BLUE);
+  Start_Tone();
+
+  // configure LED PWM functionalitites
+  for (int i = 0; i < 6; i++) {
+    ledcSetup(MOTOR_PWM_Channel_A[i], PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcSetup(MOTOR_PWM_Channel_B[i], PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcAttachPin(motorPins_A[i], MOTOR_PWM_Channel_A[i]);
+    ledcAttachPin(motorPins_B[i], MOTOR_PWM_Channel_B[i]);
+  }
+  NeoPixel_SetColour(PURPLE);
+
+  delay(1500);
+  Serial.println("CORE System Ready! ");
+  NeoPixel_SetColour(CYAN);
+}
+
+void loop() {
+
+  BP32.update();
+  if (myController && myController->isConnected()) {
+    processControllers();
+    NeoPixel_SetColour(CYAN);
+    Motion_Control(myController->axisY(), myController->axisX(), myController->axisRX());  // Joystick control
+    delay(5);
+    Motor_Control();
+  }
+  //Stop/Standby
+  else {
+    NeoPixel_SetColour(RED);
+    Motor_STOP();
+  }
+}
